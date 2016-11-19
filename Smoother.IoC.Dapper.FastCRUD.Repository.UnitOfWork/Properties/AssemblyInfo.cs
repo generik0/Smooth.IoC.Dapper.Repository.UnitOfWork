e53c89bced536c74cb5dbb24507226a7b997d2bb
Rik Svendsen Rose <rsr@gait.dk>
2016-11-19 14:17:05 +0100

    Add Solution

---
+++ Smoother.IoC.Dapper.FastCRUD.Repository.UnitOfWork/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Smoother.IoC.Dapper.FastCRUD.Repository.UnitOfWork")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e6550dc7-fc02-4b12-a7b5-ddb684a7760a")]
