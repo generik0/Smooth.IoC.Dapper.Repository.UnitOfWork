0cde79552c67fb5d3aba19f4b16795869975a3fd
Rik Svendsen Rose <rsr@gait.dk>
2016-11-19 18:32:22 +0100

    Add ITEntity with Id

---
+++ Smoother.IoC.Dapper.FastCRUD.Repository.UnitOfWork/ITEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Dapper.FastCrud;
+
+namespace Smoother.IoC.Dapper.FastCRUD.Repository.UnitOfWork
+{
+    public interface ITEntity<TPk>
+    {
+        [Key]
+        [DatabaseGeneratedDefaultValue]
+        TPk Id { get; set; }
+    }
+}
