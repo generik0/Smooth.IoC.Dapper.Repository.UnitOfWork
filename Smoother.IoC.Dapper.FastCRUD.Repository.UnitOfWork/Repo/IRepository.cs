dea1a50db127be536c1e2f1d2ac9bdd0ed0a4ae2
Rik Svendsen Rose <rsr@gait.dk>
2016-11-19 17:19:18 +0100

    Add RepositoryGetAll

--- Smoother.IoC.Dapper.FastCRUD.Repository.UnitOfWork/Repo/IRepository.cs
+++ Smoother.IoC.Dapper.FastCRUD.Repository.UnitOfWork/Repo/IRepository.cs
@@ -1,14 +1,18 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Smoother.IoC.Dapper.FastCRUD.Repository.UnitOfWork.Connection;
 using Smoother.IoC.Dapper.FastCRUD.Repository.UnitOfWork.UoW;
 
 namespace Smoother.IoC.Dapper.FastCRUD.Repository.UnitOfWork.Repo
 {
-    public interface IRepository<TSession, TEntity, TPk>
+    public interface IRepository<TSession, TEntity, in TPk>
         where TSession : ISession
         where TEntity : class
     {
         TEntity Get(TPk key, IUnitOfWork<TSession> unitOfWork = null);
         Task<TEntity> GetAsync(TPk key, IUnitOfWork<TSession> unitOfWork = null);
+        IEnumerable<TEntity>  GetAll(IUnitOfWork<TSession> unitOfWork = null);
+        Task<IEnumerable<TEntity>> GetAllAsync(IUnitOfWork<TSession> unitOfWork = null);
+
     }
 }
---
+++ Smoother.IoC.Dapper.FastCRUD.Repository.UnitOfWork/Repo/RepositoryGetAll.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+using Dapper.FastCrud;
+using Dapper.FastCrud.Configuration.StatementOptions.Builders;
+using Smoother.IoC.Dapper.FastCRUD.Repository.UnitOfWork.Connection;
+using Smoother.IoC.Dapper.FastCRUD.Repository.UnitOfWork.UoW;
+
+namespace Smoother.IoC.Dapper.FastCRUD.Repository.UnitOfWork.Repo
+{
+    public abstract partial class Repository<TSession, TEntity, TPk> where TEntity : class
+        where TSession : ISession
+    {
+        public IEnumerable<TEntity> GetAll(IUnitOfWork<TSession> unitOfWork = null)
+        {
+            return GetAllAsync(unitOfWork).Result;
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync(IUnitOfWork<TSession> unitOfWork = null)
+        {
+            return await _GetAllAsync(unitOfWork);
+        }
+        
+        protected async Task<IEnumerable<TEntity>> _GetAllAsync(IDbConnection connection = null)
+        {
+            if (connection != null)
+            {
+                return await connection.FindAsync<TEntity>();
+            }
+            using (var uow = Factory.Create<IUnitOfWork<ISession>>())
+            {
+                return await uow.FindAsync<TEntity>();
+            }
+        }
+    }
+}
