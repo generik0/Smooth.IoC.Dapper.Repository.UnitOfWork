8f413eed9b14640dddacfc52d5979b45af13272d
Rik Svendsen Rose <rsr@gait.dk>
2016-11-19 16:46:22 +0100

    Add helper

---
+++ Smoother.IoC.Dapper.FastCRUD.Repository.UnitOfWork/Helpers/CreateInstanceHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Smoother.IoC.Dapper.FastCRUD.Repository.UnitOfWork.Helpers
+{
+    public static class CreateInstanceHelper
+    {
+        public static T Resolve<T>(params object[] parameters) where T : class
+        {
+            return (T)Activator.CreateInstance(typeof(T), parameters);
+        }
+    }
+}
