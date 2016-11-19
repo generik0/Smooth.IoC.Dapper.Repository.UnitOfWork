627caed3ab17b70815c16d0e434478a3926f5100
Rik Svendsen Rose <rsr@gait.dk>
2016-11-19 18:33:06 +0100

    Add SaveOrUpdate

--- Smoother.IoC.Dapper.FastCRUD.Repository.UnitOfWork/Repo/IRepository.cs
+++ Smoother.IoC.Dapper.FastCRUD.Repository.UnitOfWork/Repo/IRepository.cs
@@ -5,14 +5,15 @@ using Smoother.IoC.Dapper.FastCRUD.Repository.UnitOfWork.UoW;
 
 namespace Smoother.IoC.Dapper.FastCRUD.Repository.UnitOfWork.Repo
 {
-    public interface IRepository<TSession, TEntity, in TPk>
+    public interface IRepository<TSession, TEntity, TPk>
+        where TEntity : class, ITEntity<TPk>
         where TSession : ISession
-        where TEntity : class
     {
         TEntity Get(TPk key, IUnitOfWork<TSession> unitOfWork = null);
         Task<TEntity> GetAsync(TPk key, IUnitOfWork<TSession> unitOfWork = null);
         IEnumerable<TEntity>  GetAll(IUnitOfWork<TSession> unitOfWork = null);
         Task<IEnumerable<TEntity>> GetAllAsync(IUnitOfWork<TSession> unitOfWork = null);
-
+        TPk SaveOrUpdate(TEntity entity, IUnitOfWork<TSession> unitOfWork = null);
+        Task<TPk> SaveOrUpdateAsync(TEntity entity, IUnitOfWork<TSession> unitOfWork = null);
     }
 }
