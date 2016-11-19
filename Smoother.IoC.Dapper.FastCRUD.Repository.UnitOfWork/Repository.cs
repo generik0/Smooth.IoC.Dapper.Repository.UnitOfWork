7e557dcc158b5a6028ed7f70edec5ddd594a95f8
Rik Svendsen Rose <rsr@gait.dk>
2016-11-19 15:02:04 +0100

    Add basic Repository

---
+++ Smoother.IoC.Dapper.FastCRUD.Repository.UnitOfWork/Repository.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Smoother.IoC.Dapper.FastCRUD.Repository.UnitOfWork.UoW;
+
+namespace Smoother.IoC.Dapper.FastCRUD.Repository.UnitOfWork
+{
+    public abstract class Repository<TSession, TEntity, TPk>
+    {
+        private readonly IUnitOfWorkFactory _factory;
+
+        protected Repository(IUnitOfWorkFactory factory)
+        {
+            _factory = factory;
+        }
+
+        public async Task<TEntity> GetAsync(TPk id, IUnitOfWork<TSession> unitOfWork = null)
+        {
+            if (unitOfWork != null)
+            {
+                return await GetCall(id, unitOfWork);
+            }
+            var uow = _factory.Create<IUnitOfWork<TSession>, TSession>().GetSession();
+
+
+        }
+
+        protected virtual async Task<TEntity> GetCall(TPk id, IUnitOfWork<TSession> unitOfWork)
+        {
+
+        }
+
+    }
+}
