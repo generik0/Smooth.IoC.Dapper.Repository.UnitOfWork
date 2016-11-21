3f4fd0caeb00f6d1d52e0062273f72cde62fdd9c
Rik Svendsen Rose <rsr@gait.dk>
2016-11-21 20:59:19 +0100

    Update AssemblyInfo

--- src/Smooth.IoC.Dapper.Repository.UnitOfWork/Properties/AssemblyInfo.cs
+++ src/Smooth.IoC.Dapper.Repository.UnitOfWork/Properties/AssemblyInfo.cs
@@ -1,12 +1,11 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright © Rik Svendsen Rose 2016")]
 [assembly: AssemblyProduct("Smoother.IoC.Dapper.FastCRUD.Repository.UnitOfWork")]
 [assembly: AssemblyTrademark("")]
 
@@ -16,4 +15,8 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e6550dc7-fc02-4b12-a7b5-ddb684a7760a")]
+#if NET_46_OR_GREATER
+[assembly: Guid("e6550dc7-fc02-4b12-a7b5-ddb684a7760a")]  
+#else
+[assembly: Guid("f9cc3500-59c4-4be2-ab64-7122dc36e4bf")]
+#endif
