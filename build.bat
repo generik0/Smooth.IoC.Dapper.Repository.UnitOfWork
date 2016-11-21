caff2d581a563cfd041c6812d54c358151824252
generik0 <rsr@gait.dk>
2016-11-21 18:53:43 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -11,7 +11,3 @@ if not "%PackageVersion%" == "" (
 
 REM Build
 "%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" Smooth.IoC.Dapper.Repository.UnitOfWork.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
-
-REM Package
-mkdir Build
-call %nuget% pack "Smooth.IoC.Dapper.Repository.UnitOfWork\Smooth.IoC.Dapper.Repository.UnitOfWork.xproj" -IncludeReferencedProjects -o Build -p Configuration=%config% %version%
