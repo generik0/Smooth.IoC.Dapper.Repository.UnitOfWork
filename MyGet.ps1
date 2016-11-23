e15d5c313293b91c00897ceef60cc2f44be09716
Rik Svendsen Rose <rsr@gait.dk>
2016-11-23 20:20:23 +0100

    Add MyGet.Include to sln

--- MyGet.ps1
+++ MyGet.ps1
@@ -15,7 +15,7 @@ MyGet-Write-Diagnostic ""
 MyGet-Write-Diagnostic "###### Run all tests ######"
 & "$PSScriptRoot\.nuget\nuget.exe" restore "$PSScriptRoot\Smooth.IoC.Dapper.Repository.UnitOfWork.sln"
 if ($LASTEXITCODE -ne 0){
-    throw "nuget restore failed"
+    MyGet-Die "nuget restore failed"
 }
 
 MyGet-Write-Diagnostic ""
@@ -23,7 +23,7 @@ MyGet-Write-Diagnostic ""
 MyGet-Write-Diagnostic "###### build the solution ######"
 & "$msBuildExe" "$PSScriptRoot\Smooth.IoC.Dapper.Repository.UnitOfWork.sln" /t:"$msBuildTarget" /p:Configuration="$configuration"
 if ($LASTEXITCODE -ne 0){
-    throw "build failed"
+    MyGet-Die "build failed"
 }
 
 MyGet-Write-Diagnostic ""
@@ -31,7 +31,7 @@ MyGet-Write-Diagnostic ""
 MyGet-Write-Diagnostic "###### Run all tests ######"
 & "$PSScriptRoot\.nunit\nunit3-console.exe" "$PSScriptRoot\src\Smooth.IoC.Dapper.Repository.UnitOfWork.Tests\bin\$configuration\Smooth.IoC.Dapper.Repository.UnitOfWork.Tests.dll"
 if ($LASTEXITCODE -ne 0){
-    throw "tests failed"
+    MyGet-Die "tests failed"
 }
 
 MyGet-Write-Diagnostic ""
@@ -39,6 +39,6 @@ MyGet-Write-Diagnostic ""
 MyGet-Write-Diagnostic "###### Create the NuGet packages ######"
 & "$PSScriptRoot\.nuget\nuget.exe" pack "$PSScriptRoot\NuGetSpecs\Smooth.IoC.Dapper.Repository.UnitOfWork.nuspec" -OutputDirectory "$PSScriptRoot\Releases" -Version "$packageVersion" -Properties configuration="$configuration" -Verbosity detailed
 if ($LASTEXITCODE -ne 0){
-    throw "Nuget library packaging failed"
+    MyGet-Die "Nuget library packaging failed"
 }
-
+MyGet-Build-Success
