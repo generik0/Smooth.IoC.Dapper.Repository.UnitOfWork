8644efee7be0edc9ed5c1fb53c41be02c6b2f629
Rik Svendsen Rose <rsr@gait.dk>
2016-11-23 20:28:04 +0100

    Added more room

--- MyGet.ps1
+++ MyGet.ps1
@@ -12,32 +12,32 @@ $rootFolder = Split-Path -parent $script:MyInvocation.MyCommand.Path
 # Valid build runners
 $validBuildRunners = @("myget")
 
-MyGet-Write-Diagnostic ""
-MyGet-Write-Diagnostic ""
+MyGet-Write-Diagnostic " "
+MyGet-Write-Diagnostic " "
 MyGet-Write-Diagnostic "###### Run all tests ######"
 & "$PSScriptRoot\.nuget\nuget.exe" restore "$PSScriptRoot\Smooth.IoC.Dapper.Repository.UnitOfWork.sln"
 if ($LASTEXITCODE -ne 0){
     MyGet-Die "nuget restore failed"
 }
 
-MyGet-Write-Diagnostic ""
-MyGet-Write-Diagnostic ""
+MyGet-Write-Diagnostic " "
+MyGet-Write-Diagnostic " "
 MyGet-Write-Diagnostic "###### build the solution ######"
 & "$msBuildExe" "$PSScriptRoot\Smooth.IoC.Dapper.Repository.UnitOfWork.sln" /t:"$msBuildTarget" /p:Configuration="$configuration"
 if ($LASTEXITCODE -ne 0){
     MyGet-Die "build failed"
 }
 
-MyGet-Write-Diagnostic ""
-MyGet-Write-Diagnostic ""
+MyGet-Write-Diagnostic " "
+MyGet-Write-Diagnostic " "
 MyGet-Write-Diagnostic "###### Run all tests ######"
 & "$PSScriptRoot\.nunit\nunit3-console.exe" "$PSScriptRoot\src\Smooth.IoC.Dapper.Repository.UnitOfWork.Tests\bin\$configuration\Smooth.IoC.Dapper.Repository.UnitOfWork.Tests.dll"
 if ($LASTEXITCODE -ne 0){
     MyGet-Die "tests failed"
 }
 
-MyGet-Write-Diagnostic ""
-MyGet-Write-Diagnostic ""
+MyGet-Write-Diagnostic " "
+MyGet-Write-Diagnostic " "
 MyGet-Write-Diagnostic "###### Create the NuGet packages ######"
 & "$PSScriptRoot\.nuget\nuget.exe" pack "$PSScriptRoot\NuGetSpecs\Smooth.IoC.Dapper.Repository.UnitOfWork.nuspec" -OutputDirectory "$PSScriptRoot\Releases" -Version "$packageVersion" -Properties configuration="$configuration" -Verbosity detailed
 if ($LASTEXITCODE -ne 0){
