d4b57b29781ae3bfcd75a95f4a090c20fb25d0ab
Rik Svendsen Rose <rsr@gait.dk>
2016-11-23 20:02:35 +0100

    Add some more nunit runnering stuff

--- MyGet.ps1
+++ MyGet.ps1
@@ -5,24 +5,33 @@ $configuration = ($env:Configuration)
 $msBuildExe = ($env:MsBuildExe)
 $msBuildTarget = ($env:Targets)
 
-# restore dependencies
+MyGet-Write-Diagnostic ""
+MyGet-Write-Diagnostic ""
+MyGet-Write-Diagnostic "###### Run all tests ######"
 & "$PSScriptRoot\.nuget\nuget.exe" restore "$PSScriptRoot\Smooth.IoC.Dapper.Repository.UnitOfWork.sln"
 if ($LASTEXITCODE -ne 0){
     throw "nuget restore failed"
 }
 
-# build the solution
+MyGet-Write-Diagnostic ""
+MyGet-Write-Diagnostic ""
+MyGet-Write-Diagnostic "###### build the solution ######"
 & "$msBuildExe" "$PSScriptRoot\Smooth.IoC.Dapper.Repository.UnitOfWork.sln" /t:"$msBuildTarget" /p:Configuration="$configuration"
 if ($LASTEXITCODE -ne 0){
     throw "build failed"
 }
 
+MyGet-Write-Diagnostic ""
+MyGet-Write-Diagnostic ""
+MyGet-Write-Diagnostic "###### Run all tests ######"
 & "$PSScriptRoot\.nunit\nunit3-console.exe" "$PSScriptRoot\src\Smooth.IoC.Dapper.Repository.UnitOfWork.Tests\bin\$configuration\Smooth.IoC.Dapper.Repository.UnitOfWork.Tests.dll"
 if ($LASTEXITCODE -ne 0){
     throw "tests failed"
 }
 
-# create the NuGet packages
+MyGet-Write-Diagnostic ""
+MyGet-Write-Diagnostic ""
+MyGet-Write-Diagnostic "###### create the NuGet packages ######"
 & "$PSScriptRoot\.nuget\nuget.exe" pack "$PSScriptRoot\NuGetSpecs\Smooth.IoC.Dapper.Repository.UnitOfWork.nuspec" -OutputDirectory "$PSScriptRoot\Releases" -Version "$packageVersion" -Properties configuration="$configuration" -Verbosity detailed
 if ($LASTEXITCODE -ne 0){
     throw "Nuget library packaging failed"
