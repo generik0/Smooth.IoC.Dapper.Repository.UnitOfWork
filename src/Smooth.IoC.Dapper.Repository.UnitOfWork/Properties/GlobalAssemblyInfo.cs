728f53d5fc5ab820a5f90fb110a278df224ac846
Rik Svendsen Rose <rsr@gait.dk>
2016-11-23 19:34:11 +0100

    Changed the version types

---
+++ src/Smooth.IoC.Dapper.Repository.UnitOfWork/Properties/GlobalAssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyCompany("Generic Automation adn IT")]
+[assembly: AssemblyCopyright("Copyright ©GAIT  2016")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+
+public static class VersionInfo
+{
+    public const string Version = "0.1.0";
+}
