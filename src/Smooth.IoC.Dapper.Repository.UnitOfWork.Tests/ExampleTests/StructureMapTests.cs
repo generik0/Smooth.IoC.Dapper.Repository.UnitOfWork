9a7b2e6cc510681ba1ded4e5097acd8f19ae94e7
Rik Svendsen Rose <rsr@gait.dk>
2016-11-24 15:19:44 +0100

    Add structure map

---
+++ src/Smooth.IoC.Dapper.Repository.UnitOfWork.Tests/ExampleTests/StructureMapTests.cs
@@ -0,0 +1,80 @@
+﻿using Dapper.FastCrud;
+using NUnit.Framework;
+using Smooth.IoC.Dapper.FastCRUD.Repository.UnitOfWork.Tests.IoC_Example_Installers;
+using Smooth.IoC.Dapper.FastCRUD.Repository.UnitOfWork.Tests.TestHelpers;
+using Smooth.IoC.Dapper.Repository.UnitOfWork.Data;
+using StructureMap;
+
+namespace Smooth.IoC.Dapper.FastCRUD.Repository.UnitOfWork.Tests.ExampleTests
+{
+    [TestFixture]
+    public class StructureMapTests
+    {
+        private static IContainer _container;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            if (_container == null)
+            {
+                _container = new Container();
+                Assert.DoesNotThrow(() =>
+                {
+                    new StructureMapRegistration().Register(_container);
+                    _container.Configure(c =>
+                    {
+                        
+                        c.Scan(s =>
+                        {
+                            s.AssembliesFromApplicationBaseDirectory();
+                            s.WithDefaultConventions();
+                        });
+
+                    });
+                });
+            }
+        }
+
+        [Test, Category("Integration")]
+        public static void Install_1_Resolves_ISession()
+        {
+            var dbFactory = _container.GetInstance<IDbFactory>();
+            ITestSession session = null;
+            Assert.DoesNotThrow(() => session = dbFactory.CreateSession<ITestSession>());
+            Assert.That(session, Is.Not.Null);
+        }
+
+
+        [Test, Category("Integration")]
+        public static void Install_2_Resolves_IUnitOfWork()
+        {
+            var dbFactory = _container.GetInstance<IDbFactory>();
+            using (var session = dbFactory.CreateSession<ITestSession>())
+            {
+                IUnitOfWork uow = null;
+                Assert.DoesNotThrow(()=> uow = session.UnitOfWork());
+                Assert.That(uow, Is.Not.Null);
+            }
+        }
+
+        [Test, Category("Integration")]
+        public static void Install_3_Resolves_SqlDialectCorrectly()
+        {
+            var dbFactory = _container.GetInstance<IDbFactory>();
+            using (var session = dbFactory.CreateSession<ITestSession>())
+            {
+                Assert.That(session.SqlDialect== SqlDialect.SqLite);
+                var uow = session.UnitOfWork();
+                Assert.That(uow.SqlDialect == SqlDialect.SqLite);
+            }
+        }
+
+        [Test, Category("Integration")]
+        public static void Install_4_Resolves_IBravoRepository()
+        {
+            IBraveRepository repo = null;
+            Assert.DoesNotThrow(() => repo = _container.GetInstance<IBraveRepository>());
+            Assert.That(repo, Is.Not.Null);
+        }
+    }
+}
---
+++ src/Smooth.IoC.Dapper.Repository.UnitOfWork.Tests/IoC_Example_Installers/StructureMapRegistration.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+using Smooth.IoC.Dapper.Repository.UnitOfWork.Data;
+using StructureMap;
+
+namespace Smooth.IoC.Dapper.FastCRUD.Repository.UnitOfWork.Tests.IoC_Example_Installers
+{
+    public class StructureMapRegistration
+    {
+        public void Register(IContainer container)
+        {
+            container.Configure(c=>c.For<IDbFactory>()
+            .UseIfNone<StructureMapDbFactory>().Ctor<IContainer>()
+            .Is(container).Singleton());
+        }
+
+    }
+}
