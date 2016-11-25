cd02ebf391d6c1396be8fc3aeab2a2cc86a702bd
Rik Svendsen Rose <rsr@gait.dk>
2016-11-25 23:14:13 +0100

    Begin logic and tests for autofac

---
+++ src/Smooth.IoC.Dapper.Repository.UnitOfWork.Tests/ExampleTests/AutofacTests.cs
@@ -0,0 +1,79 @@
+using System.Linq;
+using System.Reflection;
+using Autofac;
+using Autofac.Features.ResolveAnything;
+using Microsoft.Practices.Unity;
+using NUnit.Framework;
+using Smooth.IoC.Dapper.FastCRUD.Repository.UnitOfWork.Tests.IoC_Example_Installers;
+using Smooth.IoC.Dapper.FastCRUD.Repository.UnitOfWork.Tests.TestHelpers;
+using Smooth.IoC.Dapper.FastCRUD.Repository.UnitOfWork.Tests.TestHelpers.Registrations;
+using Smooth.IoC.Dapper.Repository.UnitOfWork.Data;
+
+namespace Smooth.IoC.Dapper.FastCRUD.Repository.UnitOfWork.Tests.ExampleTests
+{
+    [TestFixture]
+    public class AutofacTests
+    {
+        private static IContainer _container;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            if (_container == null)
+            {
+                var builder = new ContainerBuilder();
+                Assert.DoesNotThrow(() =>
+                {
+                    new AutofacRegistrar().Register(builder);
+                    builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly()).AsImplementedInterfaces()
+                    .Where(t => t.GetInterfaces().Any());
+                    _container = builder.Build();
+                });
+                Assert.That(_container.IsRegistered<ITestSession>(), Is.True);
+            }
+        }
+
+        [Test, Category("Integration")]
+        public static void Install_1_Resolves_ISession()
+        {
+            var dbFactory = _container.Resolve<IDbFactory>();
+            ITestSession session = null;
+            Assert.DoesNotThrow(() => session = dbFactory.Create<ITestSession>());
+            Assert.That(session, Is.Not.Null);
+        }
+
+
+        [Test, Category("Integration")]
+        public static void Install_2_Resolves_IUnitOfWork()
+        {
+            var dbFactory = _container.Resolve<IDbFactory>();
+            using (var session = dbFactory.Create<ITestSession>())
+            {
+                IUnitOfWork uow = null;
+                Assert.DoesNotThrow(()=> uow = session.UnitOfWork());
+                Assert.That(uow, Is.Not.Null);
+            }
+        }
+
+        [Test, Category("Integration")]
+        public static void Install_4_Resolves_WithSameConnection()
+        {
+            var dbFactory = _container.Resolve<IDbFactory>();
+            using (var session = dbFactory.Create<ITestSession>())
+            {
+                using (var uow = session.UnitOfWork())
+                {
+                    Assert.That(uow.Connection, Is.EqualTo(session.Connection));
+                }
+            }
+        }
+
+        [Test, Category("Integration")]
+        public static void Install_5_Resolves_IBravoRepository()
+        {
+            IBraveRepository repo = null;
+            Assert.DoesNotThrow(() => repo = _container.Resolve<IBraveRepository>());
+            Assert.That(repo, Is.Not.Null);
+        }
+    }
+}
