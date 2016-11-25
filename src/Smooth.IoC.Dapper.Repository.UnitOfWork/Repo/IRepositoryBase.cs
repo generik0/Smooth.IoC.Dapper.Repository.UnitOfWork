dfe4b6707c395f0de40d5bbb1d4db510dca79ef3
generik0 <rsr@gait.dk>
2016-11-25 09:27:36 +0100

    Update IRepo base

--- src/Smooth.IoC.Dapper.Repository.UnitOfWork/Repo/IRepositoryBase.cs
+++ src/Smooth.IoC.Dapper.Repository.UnitOfWork/Repo/IRepositoryBase.cs
@@ -5,16 +5,8 @@ using Smooth.IoC.Dapper.Repository.UnitOfWork.Entities;
 
 namespace Smooth.IoC.Dapper.Repository.UnitOfWork.Repo
 {
-    public interface IRepository<TEntity, TPk>
-        where TEntity : class, IEntity<TPk>
+    public interface IRepositoryBase
     {
-        TEntity GetKey(TPk key, ISession session = null);
-        Task<TEntity> GetKeyAsync(TPk key, ISession session = null);
-        TEntity Get(TEntity entity, ISession session = null);
-        Task<TEntity> GetAsync(TEntity entity, ISession session = null);
-        IEnumerable<TEntity>  GetAll(ISession session = null);
-        Task<IEnumerable<TEntity>> GetAllAsync(ISession session = null);
-        TPk SaveOrUpdate(TEntity entity, IUnitOfWork uow);
-        Task<TPk> SaveOrUpdateAsync(TEntity entity, IUnitOfWork uow);
+        
     }
 }
