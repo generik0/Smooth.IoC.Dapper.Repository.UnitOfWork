a7e541a941f4068a4f5d66d3a607663d6dbe82cd
Rik Svendsen Rose <rsr@gait.dk>
2016-11-26 11:37:43 +0100

    Add interface as resharper forgot

--- src/Smooth.IoC.Dapper.Repository.UnitOfWork.Tests/TestHelpers/MyDatabaseSettings.cs
+++ src/Smooth.IoC.Dapper.Repository.UnitOfWork.Tests/TestHelpers/MyDatabaseSettings.cs
@@ -2,8 +2,8 @@
 
 namespace Smooth.IoC.Dapper.FastCRUD.Repository.UnitOfWork.Tests.TestHelpers
 {
-    public class MyDatabaseSettings
+    public class MyDatabaseSettings : IMyDatabaseSettings
     {
-        public string ConnectionString { get; } = $@"{TestContext.CurrentContext.TestDirectory}\Tests.db";
+        public string ConnectionString { get; } = $@"Data Source={TestContext.CurrentContext.TestDirectory}\Tests.db;Version=3;New=True;BinaryGUID=False;";
     }
 }
