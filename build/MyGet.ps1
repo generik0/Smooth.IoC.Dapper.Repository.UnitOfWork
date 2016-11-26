bd37e7c05fc233bc8078b73d573aa12077967f26
Rik Svendsen Rose <rsr@gait.dk>
2016-11-26 12:11:41 +0100

    Moved build to build folder. And added local build.

---
+++ build/MyGet.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = "Stop"
+
+$packageVersion = ($env:PackageVersion)
+$configuration = ($env:Configuration)
+$msBuildExe = ($env:MsBuildExe)
+$msBuildTarget = ($env:Targets)
+
+$CommandDirectory = split-path -parent $script:MyInvocation.MyCommand.Path
+set-location "$workingDirectory"
+cd ../
+$rootFolder = (Get-Location).Path
+Write-Host Path is $rootFolder
+. $CommandDirectory\MyGet.Build.ps1
