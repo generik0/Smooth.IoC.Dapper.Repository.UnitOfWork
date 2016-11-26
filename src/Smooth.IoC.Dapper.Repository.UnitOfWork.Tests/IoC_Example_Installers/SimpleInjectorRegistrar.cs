c6175f60381316bfe17918decde5e5f468c1371c
Rik Svendsen Rose <rsr@gait.dk>
2016-11-26 12:48:47 +0100

    Begin adding simple injector

---
+++ src/Smooth.IoC.Dapper.Repository.UnitOfWork.Tests/IoC_Example_Installers/SimpleInjectorRegistrar.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data;
+using SimpleInjector;
+using Smooth.IoC.Dapper.FastCRUD.Repository.UnitOfWork.Tests.TestHelpers;
+using Smooth.IoC.Dapper.Repository.UnitOfWork.Data;
+
+namespace Smooth.IoC.Dapper.FastCRUD.Repository.UnitOfWork.Tests.IoC_Example_Installers
+{
+    public class SimpleInjectorRegistrar
+    {
+        public void Register(Container container)
+        {
+            
+        }
+    }
+
+    [NoIoCFluentRegistration]
+    internal sealed class SimpleInjectorDbFactory : IDbFactory
+    {
+        private readonly Container _container;
+
+        public SimpleInjectorDbFactory(Container container)
+        {
+            _container = container;
+        }
+
+        public T Create<T>() where T : class, ISession
+        {
+            return _container.GetInstance<T>();
+        }
+
+        public T CreateSession<T>() where T : class, ISession
+        {
+            return _container.GetInstance<T>();
+        }
+
+        public T Create<T>(IDbFactory factory, ISession session) where T : class, IUnitOfWork
+        {
+            return _container.GetInstance<T>();
+        }
+
+        public T Create<T>(IDbFactory factory, ISession session, IsolationLevel isolationLevel) where T : class, IUnitOfWork
+        {
+            return _container.GetInstance<T>();
+        }
+
+        public void Release(IDisposable instance)
+        {
+            instance?.Dispose();
+        }
+    }
+}
