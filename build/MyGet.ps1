98af57b1aaa42e2ae35c1e9bdff53df84fe41393
Rik Svendsen Rose <rsr@gait.dk>
2016-11-26 12:24:41 +0100

    Fix wrong syntax to get SourcesPath

--- build/MyGet.ps1
+++ build/MyGet.ps1
@@ -6,6 +6,5 @@ $msBuildExe = ($env:MsBuildExe)
 $msBuildTarget = ($env:Targets)
 
 $CommandDirectory = split-path -parent $script:MyInvocation.MyCommand.Path
-$rootFolder = $SourcesPath
-Write-Host Path is $rootFolder
+$rootFolder = ($env:SourcesPath)
 . $CommandDirectory\MyGet.Build.ps1
