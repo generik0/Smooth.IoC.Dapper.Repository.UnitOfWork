ecc66d5cb60127a314692ecf12c6363a3f378061
Rik Svendsen Rose <rsr@gait.dk>
2016-12-02 17:41:08 +0100

    Removed unneded calls

--- src/Smooth.IoC.Dapper.Repository.UnitOfWork/Repo/Repository.cs
+++ src/Smooth.IoC.Dapper.Repository.UnitOfWork/Repo/Repository.cs
@@ -1,15 +1,14 @@
-﻿using Dapper.FastCrud.Mappings;
-using Smooth.IoC.Dapper.Repository.UnitOfWork.Data;
+﻿using Smooth.IoC.Dapper.Repository.UnitOfWork.Data;
 using Smooth.IoC.Dapper.Repository.UnitOfWork.Entities;
 
 namespace Smooth.IoC.Dapper.Repository.UnitOfWork.Repo
 {
-    public abstract partial class Repository<TSession, TEntity, TPk> : RepositoryBase<TEntity>, IRepository<TEntity, TPk>
+    public abstract partial class Repository<TSession, TEntity, TPk> : RepositoryBase, IRepository<TEntity, TPk>
         where TEntity : class, IEntity<TPk>
         where TSession : class, ISession
     {
         protected readonly IDbFactory Factory;
-        private EntityMapping<TEntity> _mapping;
+
         protected Repository(IDbFactory factory)
         {
             Factory = factory;
