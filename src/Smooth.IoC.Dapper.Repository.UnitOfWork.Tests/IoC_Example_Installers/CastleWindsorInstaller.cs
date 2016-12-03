8dfdba218ff5fcd78a9769f9293df7167386df02
Rik Svendsen Rose <rsr@gait.dk>
2016-12-03 14:40:31 +0100

    Refractor

--- src/Smooth.IoC.Dapper.Repository.UnitOfWork.Tests/IoC_Example_Installers/CastleWindsorInstaller.cs
+++ src/Smooth.IoC.Dapper.Repository.UnitOfWork.Tests/IoC_Example_Installers/CastleWindsorInstaller.cs
@@ -22,7 +22,6 @@ namespace Smooth.IoC.Dapper.FastCRUD.Repository.UnitOfWork.Tests.IoC_Example_Ins
             }
             container.Register(Component.For<IUnitOfWork>()
                 .ImplementedBy<Dapper.Repository.UnitOfWork.Data.UnitOfWork>().IsFallback().LifestyleTransient());
-
             container.Register(Component.For<DbFactoryComponentSelector, ITypedFactoryComponentSelector>());
             container.Register(Component.For<IDbFactory>().AsFactory(c => c.SelectedWith<DbFactoryComponentSelector>()));
         }
@@ -35,23 +34,21 @@ namespace Smooth.IoC.Dapper.FastCRUD.Repository.UnitOfWork.Tests.IoC_Example_Ins
             {
                 _kernel = kernel;
             }
-
             protected override IDictionary GetArguments(MethodInfo method, object[] arguments)
             {
                 var generics = method.GetGenericArguments();
-                if (generics.Length != 2 || !method.Name.Equals("create", StringComparison.InvariantCultureIgnoreCase))
+                if (generics.Length != 2 || generics.First() != typeof(IUnitOfWork) || !method.Name.Equals("create", StringComparison.InvariantCultureIgnoreCase))
                 {
                     return base.GetArguments(method, arguments);
                 }
-                var arguements = new Arguments
+                var uow = new Arguments
                 {
                     {"session", _kernel.Resolve(generics.Last())},
                     {"isolationLevel", arguments[0]},
                     {"sessionOnlyForThisUnitOfWork", true}
                 };
-                return arguements;
+                return uow;
             }
         }
     }
-    
 }
