3ad687023bc4abf4133d26476e9d930c6d952247
Rik Svendsen Rose <rsr@gait.dk>
2016-12-03 11:32:52 +0100

    Expand interface to be more testable. So methods must have a session (i.e. not null) and add a generic for session when no session is needed.

--- src/Smooth.IoC.Dapper.Repository.UnitOfWork/Repo/IRepository.cs
+++ src/Smooth.IoC.Dapper.Repository.UnitOfWork/Repo/IRepository.cs
@@ -8,12 +8,20 @@ namespace Smooth.IoC.Dapper.Repository.UnitOfWork.Repo
         where TEntity : class
     {
         TEntity GetKey(TPk key, ISession session);
-        Task<TEntity> GetKeyAsync(TPk key, ISession session );
+        TEntity GetKey<TSesssion>(TPk key) where TSesssion : class, ISession;
+        Task<TEntity> GetKeyAsync(TPk key, ISession session);
+        Task<TEntity> GetKeyAsync<TSesssion>(TPk key) where TSesssion : class, ISession;
         TEntity Get(TEntity entity, ISession session);
+        TEntity Get<TSesssion>(TEntity entity) where TSesssion : class, ISession;
         Task<TEntity> GetAsync(TEntity entity, ISession session);
+        Task<TEntity> GetAsync<TSesssion>(TEntity entity) where TSesssion : class, ISession;
         IEnumerable<TEntity>  GetAll(ISession session);
+        IEnumerable<TEntity> GetAll<TSesssion>() where TSesssion : class, ISession;
         Task<IEnumerable<TEntity>> GetAllAsync(ISession session);
+        Task<IEnumerable<TEntity>> GetAllAsync<TSesssion>() where TSesssion : class, ISession;
         TPk SaveOrUpdate(TEntity entity, IUnitOfWork uow);
+        TPk SaveOrUpdate<TSesssion>(TEntity entity) where TSesssion : class, ISession;
         Task<TPk> SaveOrUpdateAsync(TEntity entity, IUnitOfWork uow);
+        Task<TPk> SaveOrUpdateAsync<TSesssion>(TEntity entity) where TSesssion : class, ISession;
     }
 }
