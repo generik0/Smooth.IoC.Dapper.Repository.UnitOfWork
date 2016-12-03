53c27a6a14a6ab01098ba9eb8d0148b751441f37
Rik Svendsen Rose <rsr@gait.dk>
2016-12-03 14:14:51 +0100

    Remove obserlete method

--- src/Smooth.IoC.Dapper.Repository.UnitOfWork.Tests/IoC_Example_Installers/AutofacRegistrar.cs
+++ src/Smooth.IoC.Dapper.Repository.UnitOfWork.Tests/IoC_Example_Installers/AutofacRegistrar.cs
@@ -16,41 +16,29 @@ namespace Smooth.IoC.Dapper.FastCRUD.Repository.UnitOfWork.Tests.IoC_Example_Ins
         sealed class AutofacDbFactory : IDbFactory
         {
             private readonly IComponentContext _container;
-
             public AutofacDbFactory(IComponentContext container)
             {
                 _container = container;
             }
-
             public T Create<T>() where T : class, ISession
             {
                 return _container.Resolve<T>();
             }
-
-            public T CreateSession<T>() where T : class, ISession
-            {
-                return _container.Resolve<T>();
-            }
-            
             public TUnitOfWork Create<TUnitOfWork, TSession>(IsolationLevel isolationLevel = IsolationLevel.Serializable) where TUnitOfWork : class, IUnitOfWork where TSession : class, ISession
             {
                 return _container.Resolve<TUnitOfWork>(new NamedParameter("factory", _container.Resolve <IDbFactory>()),
                     new NamedParameter("session", Create<TSession>()), new NamedParameter("isolationLevel", isolationLevel)
                     , new NamedParameter("sessionOnlyForThisUnitOfWork", true));
             }
-
             public T Create<T>(IDbFactory factory, ISession session, IsolationLevel isolationLevel = IsolationLevel.Serializable) where T : class, IUnitOfWork
             {
                 return _container.Resolve<T>(new NamedParameter("factory", factory),
                     new NamedParameter("session", session), new NamedParameter("isolationLevel", isolationLevel));
             }
-
             public void Release(IDisposable instance)
             {
                 instance.Dispose();
             }
-
-            
         }
     }
 }
