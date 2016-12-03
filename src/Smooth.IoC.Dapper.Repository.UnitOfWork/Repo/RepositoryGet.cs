5e6461f8fb9c1ddc001dacf60bfd2402cae91c7b
Rik Svendsen Rose <rsr@gait.dk>
2016-12-03 11:34:54 +0100

    Add IRepository changes to the RepositoryGet

--- src/Smooth.IoC.Dapper.Repository.UnitOfWork/Repo/RepositoryGet.cs
+++ src/Smooth.IoC.Dapper.Repository.UnitOfWork/Repo/RepositoryGet.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Smooth.IoC.Dapper.Repository.UnitOfWork.Data;
 using Smooth.IoC.Dapper.Repository.UnitOfWork.Helpers;
 
@@ -8,24 +9,63 @@ namespace Smooth.IoC.Dapper.Repository.UnitOfWork.Repo
     {
         public TEntity GetKey(TPk key, ISession session)
         {
-            return GetKeyAsync(key, session).Result;
+            var entity = CreateEntityAndSetKeyValue(key);
+            return session.Get(entity);
+        }
+
+        public TEntity GetKey<TSesssion>(TPk key) where TSesssion : class, ISession
+        {
+            using (var session = Factory.Create<TSesssion>())
+            {
+                return GetKey(key, session);
+            }
         }
 
         public async Task<TEntity> GetKeyAsync(TPk key, ISession session)
         {
-             var entity = CreateInstanceHelper.Resolve<TEntity>();
-            SetPrimaryKeyValue(entity, key) ;
+            var entity = CreateEntityAndSetKeyValue(key);
             return await GetAsync(entity, session);
         }
 
+        public Task<TEntity> GetKeyAsync<TSesssion>(TPk key) where TSesssion : class, ISession
+        {
+            using (var session = Factory.Create<TSesssion>())
+            {
+                return GetKeyAsync(key, session);
+            }
+        }
+        
         public TEntity Get(TEntity entity, ISession session)
         {
-            return GetAsync(entity, session).Result;
+            return session.Get(entity);
+        }
+
+        public TEntity Get<TSesssion>(TEntity entity) where TSesssion : class, ISession
+        {
+            using (var session = Factory.Create<TSesssion>())
+            {
+                return Get(entity, session);
+            }
         }
 
         public async Task<TEntity> GetAsync(TEntity entity, ISession session)
         {
             return await session.GetAsync(entity);
+        }
+
+        public Task<TEntity> GetAsync<TSesssion>(TEntity entity) where TSesssion : class, ISession
+        {
+            using (var session = Factory.Create<TSesssion>())
+            {
+                return GetAsync(entity, session);
+            }
+        }
+
+        private TEntity CreateEntityAndSetKeyValue(TPk key)
+        {
+            var entity = CreateInstanceHelper.Resolve<TEntity>();
+            SetPrimaryKeyValue(entity, key);
+            return entity;
         }
     }
 }
