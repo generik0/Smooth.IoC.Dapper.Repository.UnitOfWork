d928b761393d995193c5b83897141dd7c0d19382
Rik Svendsen Rose <rsr@gait.dk>
2016-12-21 12:37:28 +0100

    Made buildscript rebiuld

--- build/Build.Locally.ps1
+++ build/Build.Locally.ps1
@@ -3,7 +3,7 @@
 $packageVersion = "0.0.0-C1"
 $configuration = "Release"
 $msBuildExe = "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
-$msBuildTarget = "Build"
+$msBuildTarget = "ReBuild"
 
 $CommandDirectory = split-path -parent $MyInvocation.InvocationName
 set-location "$CommandDirectory"
